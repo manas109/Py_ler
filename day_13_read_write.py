6a8b0cc4bb1fa3a4e6f1d958b0f65af86f47aa2d
manas109 <manaswini.mm7@gmail.com>
2021-11-24 11:13:18 +0530

    R and W

--- day_13_read_write.py
+++ day_13_read_write.py
@@ -17,7 +17,20 @@ with open('trial_2.txt','a') as f:
     f.write('ha')
     
     #f.write('Haaaaaa_1')
+
+
+d={1:2}
     
-    
-    
+import pickle
+with open('model.pkl', 'wb') as f:
+    pickle.dump(d,f)
+
+with open('model.pkl', 'rb') as f:
+    fi =pickle.load(f)
+
+d={"name":"manu","dob":"jun","num":"9900662117"}
+with open('model_01.pkl', 'wb') as f:
+    pickle.dump(d,f)
+with open('model_01.pkl','rb')as f:
+    gi=pickle.load(f)
     
